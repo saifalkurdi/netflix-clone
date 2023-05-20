371d827185d0bfd6caa595d0888360bd9ba74449
saifalkurdi <saifalkurdi66@gmail.com>
2023-05-20 11:17:52 +0000

    creating netflix application.

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,20 +1,24 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
 
-function MovieCard({result, handelShow, setSingleResult}) {
+function MovieCard({ result, handleShow, setSingleResult }) {
   const handleClick = () => {
-    setSingleResult(result)
-    handelShow()
-  }
+    setSingleResult(result);
+    handleShow();
+  };
+
   return (
-    <Card style={{ width: '18rem' }} key={result.id}>
-      <Card.Img variant="top" src={result.backdrop_path} />
+    <Card style={{ width: "18rem" }} key={result.id}>
+      <Card.Img variant="top" alt={result.title} src={result.backdrop_path} />
       <Card.Body>
         <Card.Title>{result.title}</Card.Title>
-        <Card.Text>
-          {result.overview}
-        </Card.Text>
-        <Button variant="primary" onClick={handleClick}>Add To Favorite List</Button>
+        <Card.Text>{result.overview}</Card.Text>
+    
+        <Button variant="primary" onClick={handleClick}>
+          Add To Favorite List
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Row } from "react-bootstrap";
+import MovieCard from "./Movie";
 
-export default function MovieList() {
+export default function MovieList({ movies, handleShow, setSingleResult }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Row>
+      {movies.length &&
+        movies.map((item) => (
+          <MovieCard
+            result={item}
+            handleShow={handleShow}
+            setSingleResult={setSingleResult}
+          />
+        ))}
+    </Row>
+  );
 }
+
